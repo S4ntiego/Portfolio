c2530414a0fb426b6236d7e1b8a210b9e1d0b193
S4ntiego <adamksiazek123@gmail.com>
2023-05-03 15:52:35 +0200

    deleted hello and skills api routes

--- src/app/api/skills/route.ts
+++
@@ -1,6 +0,0 @@
-import skills from "./data.json";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export async function GET(res: NextApiResponse, req: NextApiRequest) {
-  return res.json(skills);
-}

1e3d9ddcce32638300ed1a6d65cedb6930147af6
S4ntiego <adamksiazek123@gmail.com>
2023-05-03 12:33:06 +0200

    fixed module typos in navbar and footer imports

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./navbar.module.scss";
+import styles from "./Navbar.module.scss";
 import { navbarConfig } from "@/config/navbar";
 import Image from "next/image";
 
